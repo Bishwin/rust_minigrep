a0f3d91ded375cae889b50260a32bc0dcedb1ac5
Jake <9053841+Bishwin@users.noreply.github.com>
2020-03-28 01:25:31 +0000

    implement search function

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -39,7 +47,7 @@ mod test {
         let query = "duct";
         let contents = "\
 Rust:
-safe, fast, productive,
+safe, fast, productive.
 Pick three.";
 
         assert_eq!(
